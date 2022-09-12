c90eb1e0ebfbcbc3f505231393d47b8dbe2c7f02
HongBeen <redbean-2@naver.com>
2022-09-12 22:39:51 +0900

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from "next/app";
+import {ThemeProvider, GlobalStyle} from "../styles";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+	return (
+		<ThemeProvider>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
